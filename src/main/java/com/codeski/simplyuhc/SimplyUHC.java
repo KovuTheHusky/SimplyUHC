710b3a9936b74ed4f89d965d3b305d1f1d936927
Kevin Breslin <kevin.breslin@icloud.com>
2014-12-10 12:19:34 -0500

    Update to use 1.8 features like Spectator mode

--- src/main/java/com/codeski/simplyuhc/SimplyUHC.java
+++ src/main/java/com/codeski/simplyuhc/SimplyUHC.java
@@ -1,30 +1,31 @@
 package com.codeski.simplyuhc;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.GameMode;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.event.player.PlayerLoginEvent.Result;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
@@ -39,7 +40,7 @@ public class SimplyUHC extends JavaPlugin implements Listener {
 			if (count > 0)
 				server.broadcastMessage(10 * count + " minutes passed.");
 			else {
-				Player[] players = server.getOnlinePlayers();
+				players = new ArrayList<Player>(server.getOnlinePlayers());
 				for (Player p : players) {
 					p.setGameMode(GameMode.SURVIVAL);
 					SimplyUHC.this.healPlayer(p);
@@ -68,9 +69,9 @@ public class SimplyUHC extends JavaPlugin implements Listener {
 	private FileConfiguration configuration;
 	private CommandSender console;
 	private final PotionEffectType[] effects = { PotionEffectType.SLOW, PotionEffectType.SLOW_DIGGING, PotionEffectType.DAMAGE_RESISTANCE, PotionEffectType.INVISIBILITY, PotionEffectType.BLINDNESS };
+	private boolean inProgress = false;
 	private ArrayList<Player> players;
 	private Server server;
-	private boolean started = false;
 	private Timer timer;
 	private World world;
 
@@ -83,8 +84,10 @@ public class SimplyUHC extends JavaPlugin implements Listener {
 				return true;
 			}
 			else if (args[0].equalsIgnoreCase("start")) {
-				if (started)
+				if (inProgress)
 					sender.sendMessage("There is already a game in progress.");
+				else if (server.getOnlinePlayers().size() < 2)
+					sender.sendMessage("You need at least two players to start.");
 				else {
 					int countdown = 30;
 					switch (args.length) {
@@ -94,12 +97,12 @@ public class SimplyUHC extends JavaPlugin implements Listener {
 							this.start(Math.abs(Integer.parseInt(args[1])), countdown * 20);
 							break;
 						default:
-							sender.sendMessage(ChatColor.RED + "Usage: /uhc start <radius> [countdown]");
+							sender.sendMessage(ChatColor.RED + "Usage: /uhc start <size> [countdown]");
 					}
 				}
 				return true;
 			} else if (args[0].equalsIgnoreCase("stop")) {
-				if (!started)
+				if (!inProgress)
 					sender.sendMessage("There is no game in progress.");
 				else
 					this.stop();
@@ -120,83 +123,47 @@ public class SimplyUHC extends JavaPlugin implements Listener {
 		server.getPluginManager().registerEvents(this, this);
 		server.dispatchCommand(console, "defaultgamemode survival");
 		server.dispatchCommand(console, "difficulty hard");
+		server.dispatchCommand(console, "gamerule naturalRegeneration true");
 		server.dispatchCommand(console, "gamerule doDaylightCycle false");
 		server.dispatchCommand(console, "gamerule doMobSpawning false");
 		server.dispatchCommand(console, "time set 0");
-		for (Entity entity : server.getWorld("world").getLivingEntities())
-			if (entity instanceof Monster)
-				entity.remove();
+		for (Entity e : server.getWorld("world").getLivingEntities())
+			if (e instanceof Monster)
+				e.remove();
 	}
 
 	@EventHandler
 	public void onPlayerDeath(final PlayerDeathEvent event) {
-		if (started) {
+		if (inProgress) {
 			ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
-			SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
-			skullMeta.setOwner(event.getEntity().getName());
-			skullMeta.setDisplayName(ChatColor.RESET + event.getEntity().getName());
-			skull.setItemMeta(skullMeta);
+			SkullMeta skullMetadata = (SkullMeta) skull.getItemMeta();
+			skullMetadata.setOwner(event.getEntity().getName());
+			skullMetadata.setDisplayName(ChatColor.RESET + event.getEntity().getName());
+			skull.setItemMeta(skullMetadata);
 			event.getDrops().add(skull);
 			players.remove(event.getEntity());
 			if (players.size() == 1) {
+				Firework firework = (Firework) world.spawnEntity(players.get(0).getLocation(), EntityType.FIREWORK);
+				FireworkMeta fireworkMetadata = firework.getFireworkMeta();
+				FireworkEffect fireworkEffect = FireworkEffect.builder().with(Type.BALL_LARGE).withColor(Color.YELLOW).withTrail().flicker(true).build();
+				fireworkMetadata.addEffect(fireworkEffect);
+				fireworkMetadata.setPower(2);
+				firework.setFireworkMeta(fireworkMetadata);
+				players.clear();
+				for (Player p : server.getOnlinePlayers())
+					p.setGameMode(GameMode.SPECTATOR);
 				server.broadcastMessage(players.get(0).getName() + " is the winner!");
 				this.stop();
 			}
 			else
-				event.getEntity().kickPlayer(event.getDeathMessage().replaceFirst(event.getEntity().getName(), "You") + ".");
+				event.getEntity().setGameMode(GameMode.SPECTATOR);
 		}
 	}
 
 	@EventHandler
 	public void onPlayerLogin(final PlayerLoginEvent event) {
-		if (started && !players.contains(event.getPlayer()))
-			event.disallow(Result.KICK_OTHER, "You cannot rejoin until the game is over. Sorry!");
-	}
-
-	private void buildBorder(int x0, int z0, int radius, Material type) {
-		int x = radius, z = 0;
-		int radiusError = 1 - x;
-		while (x >= z) {
-			for (int y = 0; y < world.getMaxHeight(); ++y) {
-				world.getBlockAt(x + x0, y, z + z0).setType(type);
-				world.getBlockAt(x + x0, y, z + z0).setType(type);
-				world.getBlockAt(z + x0, y, x + z0).setType(type);
-				world.getBlockAt(-x + x0, y, z + z0).setType(type);
-				world.getBlockAt(-z + x0, y, x + z0).setType(type);
-				world.getBlockAt(-x + x0, y, -z + z0).setType(type);
-				world.getBlockAt(-z + x0, y, -x + z0).setType(type);
-				world.getBlockAt(x + x0, y, -z + z0).setType(type);
-				world.getBlockAt(z + x0, y, -x + z0).setType(type);
-			}
-			++z;
-			if (radiusError < 0)
-				radiusError += 2 * z + 1;
-			else
-				radiusError += 2 * (z - --x + 1);
-		}
-	}
-
-	private Location[] buildSpawns(int x0, int z0, int radius) {
-		Location[] locations = new Location[players.size()];
-		for (int i = 0; i < players.size(); ++i) {
-			int x = (int) (Math.cos(2 * Math.PI * i / players.size()) * radius) + x0;
-			int z = (int) (Math.sin(2 * Math.PI * i / players.size()) * radius) + z0;
-			int y = world.getMaxHeight() - 1;
-			for (; world.getBlockAt(x, y, z).getType() == Material.AIR; --y)
-				;
-			Block b = world.getBlockAt(x, y, z);
-			Material m = b.getType();
-			if (m == Material.STATIONARY_LAVA || m == Material.WATER || m == Material.LAVA) {
-				b.getRelative(BlockFace.UP).setType(Material.GLASS);
-				y += 3;
-			} else if (m == Material.STATIONARY_WATER) {
-				b.getRelative(BlockFace.UP).setType(Material.WATER_LILY);
-				y += 2;
-			} else
-				y += 1;
-			locations[i] = new Location(world, x + 0.5, y + 0.5, z + 0.5);
-		}
-		return locations;
+		if (inProgress && !players.contains(event.getPlayer()))
+			event.getPlayer().setGameMode(GameMode.SPECTATOR);
 	}
 
 	private void clearInventory(Player player) {
@@ -220,28 +187,22 @@ public class SimplyUHC extends JavaPlugin implements Listener {
 		player.setHealth(player.getMaxHealth());
 	}
 
-	private void start(int radius, int countdown) {
-		this.buildBorder(world.getSpawnLocation().getBlockX(), world.getSpawnLocation().getBlockZ(), radius, Material.BEDROCK);
-		if (server.getPluginManager().isPluginEnabled("WorldBorder"))
-			server.dispatchCommand(console, "wb world set " + radius + " " + world.getSpawnLocation().getBlockX() + " " + world.getSpawnLocation().getBlockZ());
-		players = new ArrayList<Player>(Arrays.asList(server.getOnlinePlayers()));
-		Location[] locations = this.buildSpawns(world.getSpawnLocation().getBlockX(), world.getSpawnLocation().getBlockZ(), (int) (radius * 0.75));
-		for (int i = 0; i < players.size(); ++i) {
-			this.teleportPlayer(players.get(i), locations[i]);
-			this.freezePlayer(players.get(i), countdown);
-		}
+	private void start(int size, int countdown) {
+		players = new ArrayList<Player>(server.getOnlinePlayers());
+		server.dispatchCommand(console, "worldborder set " + size);
+		server.dispatchCommand(console, "spreadplayers " + world.getSpawnLocation().getBlockX() + " " + world.getSpawnLocation().getBlockZ() + " " + size / players.size() + " " + size + " true");
+		for (Player p : players)
+			this.freezePlayer(p, countdown);
 		server.broadcastMessage("Game begins in " + countdown / 20 + " seconds... Get ready!");
 		timer = new Timer();
 		timer.scheduleAtFixedRate(new SimplyTimer(), countdown / 20 * 1000, 600000);
-		started = true;
+		inProgress = true;
 	}
 
 	private void stop() {
 		server.broadcastMessage("The game has been stopped.");
-		started = false;
+		inProgress = false;
 		timer.cancel();
-		if (server.getPluginManager().isPluginEnabled("WorldBorder"))
-			server.dispatchCommand(console, "wb world clear");
 		server.dispatchCommand(console, "gamerule naturalRegeneration true");
 		server.dispatchCommand(console, "gamerule doDaylightCycle false");
 		server.dispatchCommand(console, "gamerule doMobSpawning false");
@@ -249,10 +210,6 @@ public class SimplyUHC extends JavaPlugin implements Listener {
 		server.dispatchCommand(console, "scoreboard objectives remove deaths");
 		server.dispatchCommand(console, "scoreboard objectives remove health");
 		server.dispatchCommand(console, "scoreboard objectives remove kills");
-	}
-
-	private void teleportPlayer(Player player, Location location) {
-		player.teleport(location);
 	}
 
 	private void unfreezePlayer(Player player) {
